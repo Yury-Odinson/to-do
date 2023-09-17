9f1dfa5feee236ad79c1057b1caf77e084f41866
Yury <yury.odinson@gmail.com>
2023-09-17 14:47:41 +0300

    feat(develop): rerender tasks

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,11 +1,21 @@
-import { useState } from "react"
-import { Tasks } from "./Tasks"
-import { setCurrentTask } from "../tools/storage"
+import { useEffect, useState } from "react"
+import { setCurrentTask, tasksStore } from "../tools/storage"
+import { Task } from "../tools/types"
 
 export const Main = () => {
 
     const [task, setTask] = useState("")
+    const [tasks, setTasks] = useState<Task[]>([])
+    const [loaded, setLoaded] = useState(false)
+
+    useEffect(() => {
+        if (loaded) return;
+        setTasks(JSON.parse(localStorage.getItem("tasks") || "[]"));
+        setLoaded(true);
+    }, [loaded])
+
     const completed = false
+    const id = (Math.floor(Math.random() * (100000 - 1)) + 1).toString()
 
     const clearTasks = () => localStorage.clear()
 
@@ -14,28 +24,36 @@ export const Main = () => {
             <div className="app-header">
                 <h1>todos</h1>
             </div>
-
             <div className="app-body">
-
-                <button onClick={() => setCurrentTask({ task, completed })}>add task</button>
+                <button onClick={() => {
+                    setCurrentTask({ task, completed, id });
+                    setLoaded(false);
+                }}>add task</button>
                 <input className="app-body__input" placeholder="What needs to be done?" onChange={(e) => setTask(e.target.value)} />
-
                 <div className="app-body-tasks">
-                    <Tasks />
+                    {
+                        tasks.map((element) => {
+                            return (
+                                <div className="app-body-tasks__element" id={element.id} key={element.id}>
+                                    <input className="task__checkbox" type="checkbox" defaultChecked={element.completed} />
+                                    <input className="task__input" type="text" defaultValue={element.task} readOnly />
+                                </div>
+                            )
+                        })
+                    }
                 </div>
-
             </div>
-
-
             <div className="app-footer">
                 <span>2 items</span>
                 <button>All</button>
                 <button>Active</button>
                 <button>Completed</button>
-                <button onClick={() => clearTasks()}>Clear Completed</button>
-
+                <button onClick={() => {
+                    tasksStore.length = 0;
+                    clearTasks();
+                    setLoaded(false);
+                }}>Clear Completed</button>
             </div>
-
         </div>
     )
 }
--- src/tools/storage.ts
+++ src/tools/storage.ts
@@ -2,7 +2,7 @@ import { Task } from "./types"
 
 export const tasksStore: Task[] = JSON.parse(localStorage.getItem("tasks") || "[]") || []
 
-export const setCurrentTask = ({ task, completed }: Task) => {
-    tasksStore.push({ task, completed })
+export const setCurrentTask = ({ task, completed, id }: Task) => {
+    tasksStore.push({ task, completed, id })
     return localStorage.setItem("tasks", JSON.stringify(tasksStore))
 }
